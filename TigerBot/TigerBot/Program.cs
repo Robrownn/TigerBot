9a59ae3071d8579767c5908bb5e2c9e4e87c83c5
Robert Brown <robrownn@gmail.com>
2018-01-10 11:58:21 -0500

    Created the first command, ping!

--- TigerBot/TigerBot/Program.cs
+++ TigerBot/TigerBot/Program.cs
@@ -1,18 +1,18 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Configuration;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace TigerBot
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        
 
         private string _token;
         private DiscordSocketClient _client;
@@ -31,13 +31,57 @@ namespace TigerBot
                         .AddSingleton(_commands)
                         .BuildServiceProvider();
 
-            string botPrefix = _token;
+            string botToken = _token;
 
+            // Event Subscriptions
+            _client.Log += Log;
+
+            // Register our commands
+            await RegisterCommandsAsync();
+
+            // Logs the bot in with our token
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            // Starts the bot
+            await _client.StartAsync();
+
+            // Keeps the bot from shutting off immediately
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
         }
 
         public async Task RegisterCommandsAsync()
         {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
 
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+
+
+            }
         }
     }
 }
