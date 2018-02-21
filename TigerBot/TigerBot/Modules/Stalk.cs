df75de9d97308aacf91e7174852ad5fabcf512dc
Robrownn <robrownn@gmail.com>
2018-02-21 14:43:32 -0500

    Made the embed for the stalk command look prettier

--- TigerBot/TigerBot/Modules/Stalk.cs
+++ TigerBot/TigerBot/Modules/Stalk.cs
@@ -25,9 +25,13 @@ namespace TigerBot.Modules
             var stalkMessage = await Context.Channel.SendMessageAsync($"Getting all messages from {key}...");
 
             EmbedBuilder builder = new EmbedBuilder();
-            
+            builder.Title = $"Stalking {key}...";
+            builder.Description = $"I will root out the truth from {key}'s words!";
+            builder.ThumbnailUrl = user.GetAvatarUrl();
+            builder.Author.Name = Context.Message.Author.Mention;
+            builder.Author.IconUrl = Context.Message.Author.GetAvatarUrl();
+            builder.Color = Color.Gold;
 
-            builder.AddField("Cached Messages", "All cached messages");
             foreach(var m in _msgHistory[key])
             {
                 builder.AddField($"{m.Timestamp.ToString()}: ", m.Content);
